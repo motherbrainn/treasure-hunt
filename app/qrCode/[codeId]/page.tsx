2365354426fa88f1ec631307e197ec9c7fbf97dd
Aaron Ehresman <aaron.ehr@gmail.com>
2023-05-21 18:57:54 -0700

    add typing to page

--- app/qrCode/[codeId]/page.tsx
+++ app/qrCode/[codeId]/page.tsx
@@ -1,4 +1,10 @@
-export default function Page({ params }) {
+interface PagePropsInterface {
+  params: {
+    codeId: string;
+  };
+}
+
+export default function Page({ params }: PagePropsInterface) {
   console.log(params.codeId);
   return <h1>hello</h1>;
 }
